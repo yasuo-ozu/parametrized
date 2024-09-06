0388370837c8759da6c2671bc24f5c89d29c0416
yozu <yasuo@ozu.email>
2024-09-07 00:32:13 +0900

    Add Enum test

--- tests/test_enum.rs
+++ tests/test_enum.rs
@@ -1,7 +1,6 @@
-use parametric_type::parametric_type;
-use parametric_type::type_argument::IntoIteratorOfNthArgument;
+use parametrized::*;
 
-#[parametric_type(K, iter, iter_mut, into_iter)]
+#[parametrized(default, iter_mut, into_iter)]
 enum Enum1<K> {
     V1,
     V2(K),
@@ -11,22 +10,19 @@ enum Enum1<K> {
 #[test]
 fn test1() {
     let e1: Enum1<()> = Enum1::V1;
-    assert_eq!((&e1).len_of_arg(), 0);
-    assert_eq!(
-        (&e1).into_iter_of_arg().collect::<Vec<_>>(),
-        vec![] as Vec<&()>
-    );
+    assert_eq!((&e1).param_len(), 0);
+    assert_eq!(e1.param_iter().collect::<Vec<_>>(), vec![] as Vec<&()>);
     let mut e2 = Enum1::V2(123usize);
-    assert_eq!((&e2).len_of_arg(), 1);
-    assert_eq!((&e2).into_iter_of_arg().collect::<Vec<_>>(), vec![&123]);
-    (&mut e2).into_iter_of_arg().for_each(|a| {
+    assert_eq!(e2.param_len(), 1);
+    assert_eq!(e2.param_iter().collect::<Vec<_>>(), vec![&123]);
+    e2.param_iter_mut().for_each(|a| {
         *a *= 2;
     });
-    assert_eq!((&e2).into_iter_of_arg().collect::<Vec<_>>(), vec![&246]);
-    assert_eq!(e2.into_iter_of_arg().collect::<Vec<_>>(), vec![246]);
+    assert_eq!(e2.param_iter().collect::<Vec<_>>(), vec![&246]);
+    assert_eq!(e2.param_into_iter().collect::<Vec<_>>(), vec![246]);
     let e3 = Enum1::V3 {
         _f1: 1,
         _f2: 2usize,
     };
-    assert_eq!((&e3).len_of_arg(), 1);
+    assert_eq!(e3.param_len(), 1);
 }
